f48c237780173bf6295105df8da0c6fb9696cbfc
Alexander <kuhar@mail.ru>
2022-11-09 23:44:48 +0300

    Выполнил дз 2.10 "Телефонный справочник"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        PhoneBook.addTelephoneDirectory("Иван Петров", "89607645474");
+        PhoneBook.addTelephoneDirectory("Петр Сидоров", "8945865466");
+        PhoneBook.addTelephoneDirectory("Сидор Николаев", "8676756723");
+        PhoneBook.addTelephoneDirectory("Николай Белов", "8767866754");
+        PhoneBook.addTelephoneDirectory("Глеб Шмелев", "465860");
+        PhoneBook.addTelephoneDirectory("Владимир Тупин", "780965665656");
+        PhoneBook.addTelephoneDirectory("Ярослав Мудрый", "5604367");
+        PhoneBook.addTelephoneDirectory("Андрей Смирнов", "5677688545378");
+        PhoneBook.addTelephoneDirectory("Павел Лешин", "6765680886");
+        PhoneBook.addTelephoneDirectory("Александр Невский", "677098954");
+        PhoneBook.addTelephoneDirectory("Михаил Заступов", "9678564534");
+        PhoneBook.addTelephoneDirectory("Юлия Плутнева", "56789");
+        PhoneBook.addTelephoneDirectory("Сергей Плахов", "34467879856");
+        PhoneBook.addTelephoneDirectory("Валерия Дубова", "545456564545");
+        PhoneBook.addTelephoneDirectory("Антон Филимонов", "4487907");
+        PhoneBook.addTelephoneDirectory("Георгий Мухин", "5453434566");
+        PhoneBook.addTelephoneDirectory("Ирина Голубкина", "8956889980");
+        PhoneBook.addTelephoneDirectory("Василий Ищенко", "77756489989");
+        PhoneBook.addTelephoneDirectory("Серафим Петровский", "2333556");
+        PhoneBook.addTelephoneDirectory("Анна Тяпкина", "8975656445");
+        PhoneBook.printTelephoneDirectory();
     }
 }
